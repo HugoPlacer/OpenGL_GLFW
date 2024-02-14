fc5ee04e5d7ba8cf3447a4d7c894412562598033
HugoUPC <hugo.planell@estudiantat.upc.edu>
2024-02-14 22:02:37 +0100

    vcpkg

--- src/6.Camera/Camera.cpp
+++ src/6.Camera/Camera.cpp
@@ -80,7 +80,7 @@ int main()
 
     // build and compile our shader zprogram
     // ------------------------------------
-    Shader ourShader("../src/6.Camera/shader.vs", "../src/6.Camera/shader.fs");
+    Shader ourShader("../../../src/6.Camera/shader.vs", "../../../src/6.Camera/shader.fs");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -174,7 +174,7 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char *data = stbi_load("../assets/container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("../../../assets/container.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -196,7 +196,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load("../assets/awesomeface2.png", &width, &height, &nrChannels, 0);
+    data = stbi_load("../../../assets/awesomeface2.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
