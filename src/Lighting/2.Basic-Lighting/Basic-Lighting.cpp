e57f82299a8c131da3cf0df7fbf9d4f2facf8d85
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-02-15 12:51:34 +0100

    basic-lighting first part

--- src/Lighting/2.Basic-Lighting/Basic-Lighting.cpp
+++ src/Lighting/2.Basic-Lighting/Basic-Lighting.cpp
@@ -1,6 +1,7 @@
 
 
 #include "glm/ext/matrix_transform.hpp"
+#include "glm/ext/vector_float3.hpp"
 #include "glm/fwd.hpp"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -33,6 +34,7 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
+//glm::vec3 lightPos(-0.6f, 1.0f, 2.0f);
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 /*glm::vec3 lightColor(0.0f, 1.0f, 0.0f);
@@ -161,7 +163,6 @@ float vertices[] = {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -176,6 +177,8 @@ float vertices[] = {
         // -----
         processInput(window);
 
+        //lightPos = glm::vec3(lightPos.x + sin(glfwGetTime()) * 0.01f, lightPos.y, lightPos.z);
+
         // render
         // ------
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
@@ -185,6 +188,7 @@ float vertices[] = {
         lightingShader.use();
         lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("lightPos", lightPos);
 
 
         // view/projection transformations
--- src/Lighting/2.Basic-Lighting/colors.fs
+++ src/Lighting/2.Basic-Lighting/colors.fs
@@ -5,6 +5,7 @@ uniform vec3 objectColor;
 uniform vec3 lightColor;
 
 in vec3 Normal;
+in vec3 FragPos;
 
 uniform vec3 lightPos;
 
@@ -13,6 +14,12 @@ void main()
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
 
-    vec3 result = ambient * objectColor;
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    vec3 result = (ambient + diffuse) * objectColor;
     FragColor = vec4(result, 1.0);
 }
