5e5690d2b2cc190241fc0470452108d9a87a2462
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-02-18 23:03:46 +0100

    Multiple Lights Done!

--- src/Lighting/5.Light-Casters/colors.fs
+++ src/Lighting/5.Light-Casters/colors.fs
@@ -19,12 +19,18 @@ struct Material {
 uniform Material material;
 
 struct Light {
-    //vec3 position;
+    vec3 position;
     vec3 direction;
+    float cutOff;
+    float outerCutOff;
 
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 uniform Light light;
@@ -32,8 +38,11 @@ uniform Light light;
 void main()
 {
     vec3 norm = normalize(Normal);
-    //vec3 lightDir = normalize(light.position - FragPos);
-    vec3 lightDir = normalize(-light.direction); 
+    vec3 lightDir = normalize(light.position - FragPos); 
+    //vec3 lightDir = normalize(-light.direction);
+
+    vec4 color;
+
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
     
@@ -47,5 +56,21 @@ void main()
     //vec3 emision = vec3(texture(material.emision, TexCoords));
     vec3 emision = texture(material.emision,TexCoords + vec2(0.0,u_time)).rgb * floor(vec3(1.f) - texture(material.specular,TexCoords ).rgb);
 
-    FragColor = vec4(ambient + diffuse + specular, 1.0);
+    float theta = dot(lightDir, normalize(-light.direction));
+    float epsilon = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+
+    diffuse *= intensity;
+    specular *= intensity;
+
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0f / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+    
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+
+    color = vec4(ambient + diffuse + specular, 1.0);
+
+    FragColor = color;
 }
