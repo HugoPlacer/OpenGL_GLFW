d14ab485668efcb21420fe1d971da7a94f2ef9ff
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-05-04 14:17:50 +0200

    Update Advanced-Lighting.cpp

--- src/Advanced-Lighting/1.Advanced-Lighting/Advanced-Lighting.cpp
+++ src/Advanced-Lighting/1.Advanced-Lighting/Advanced-Lighting.cpp
@@ -40,13 +40,6 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-//glm::vec3 lightPos(0.0f, 1.0f, -2.0f);
-//glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
-
-/*glm::vec3 lightColor(0.0f, 1.0f, 0.0f);
-glm::vec3 toyColor(1.0f, 0.5f, 0.31f);
-glm::vec3 result */
-
 int main()
 {
     // glfw: initialize and configure
@@ -141,17 +134,14 @@ float vertices[] = {
     -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
 };
 
-glm::vec3 cubePositions[] = {
-glm::vec3( 0.0f,  0.0f,  0.0f),
-glm::vec3( 2.0f,  5.0f, -15.0f),
-glm::vec3(-1.5f, -2.2f, -2.5f),
-glm::vec3(-3.8f, -2.0f, -12.3f),
-glm::vec3( 2.4f, -0.4f, -3.5f),
-glm::vec3(-1.7f,  3.0f, -7.5f),
-glm::vec3( 1.3f, -2.0f, -2.5f),
-glm::vec3( 1.5f,  2.0f, -2.5f),
-glm::vec3( 1.5f,  0.2f, -1.5f),
-glm::vec3(-1.3f,  1.0f, -1.5f)
+float plane[] = {
+    // positions          // normals           // texture coords
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f
 };
 
 glm::vec3 pointLightPositions[] = {
@@ -297,20 +287,18 @@ glm::vec3 pointLightPositions[] = {
         glActiveTexture(GL_TEXTURE2);
         glBindTexture(GL_TEXTURE_2D, emisionMap);
 
+        // world transformation
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::translate(model, glm::vec3(0,-5.0f,0));
+        glm::scale(model, glm::vec3(5.0f));
+
+        lightingShader.setMat4("model", model);
+
         // render the cube
         glBindVertexArray(cubeVAO);
         //glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        // world transformation
-        glm::mat4 model = glm::mat4(1.0f);
-        for(unsigned int i = 0; i < 10; i++){
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
-            model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
-            lightingShader.setMat4("model", model);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
+        glDrawArrays(GL_TRIANGLES, 30, 6);
 
         // also draw the lamp object
         lightCubeShader.use();
